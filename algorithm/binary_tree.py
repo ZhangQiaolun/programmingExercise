15b8cebc876de9a28a473d009b187bbb2af131b0
zhql0907 <zhql0907@outlook.com>
2016-10-13 22:15:48 +0800

    add functions to binary_tree.py

---
+++ algorithm/binary_tree.py
@@ -0,0 +1,50 @@
+class Node:
+    """
+    Tree node: left and right child + data which can be any object
+    """
+    def __init__(self, data):
+        """
+        Node constructor
+        @param data node data object
+        """
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+        """
+        Insert new node with data
+        @param data node data object to insert
+        """
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def lookup(self, data, parent=None):
+        """
+        Lookup node containing data
+
+        @param data node data object to look up
+        @param parent node's parent
+        @returns node and node's parent if found or None, None
+        """
+        if data < self.data:
+            if self.left is None:
+                return None, None
+            return self.left.lookup(data, self)
+        elif data > self.data:
+            if self.right is None:
+                return None, None
+            return self.right.lookup(data, self)
+        else:
+            return self, parent
